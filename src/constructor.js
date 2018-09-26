dc1672e538c42338bdaa8e83862344c7baf6953f
Luke Mizuhashi <luke.mizuhashi@nable.org>
2018-09-26 22:16:51 +0000

    Rough draft of insert and redBlackTreeRepair methods

--- src/constructor.js
+++ src/constructor.js
@@ -6,19 +6,66 @@ module.exports = class RedBlackTree extends BinarySearchTree {
     super(options);
   }
 
+  insertRepairTree(thisNode) {
+    let uncle;
+    if (thisNode.parent) {
+      uncle = thisNode.parent.getSibling();
+    }
+
+    // Case 1
+    if (!thisNode.parent) {
+      thisNode.isBlack = true;
+
+    // Case 2
+    } else if (thisNode.parent.isBlack) {
+      // Do nothing.
+
+    // Case 3
+    } else if (uncle.isRed) {
+      thisNode.parent.isBlack = true;
+      uncle.isBlack = true;
+      uncle.parent.isRed = true;
+      this.insertRepairTree(uncle.parent);
+
+    // Case 4
+    } else {
+      let p = thisNode.parent;
+      let g = thisNode.parent.parent;
+
+      if (thisNode === g.left.right) {
+        p.rotateLeft();
+        thisNode = thisNode.left;
+      } else if (thisNode === g.right.left) {
+        p.rotateRight();
+        thisNode = thisNode.right; 
+      }
+
+      p = thisNode.parent;
+      g = thisNode.parent.parent;
+      if (thisNode === p.left) {
+        g.rotateRight();
+      } else {
+        g.rotateLeft();
+        p.isBlack = true;
+        g.isRed = true;
+      }
+    }
+  }
+
   insert(key,value) {
-    let newNode = super.insert(key,value);
-    newNode.isRed = true;
+    let thisNode = super.insert(key,value);
+    thisNode.isRed = true;
 
     const newLeft = this.nodeFactory({ isRed: false });
     const newRight = this.nodeFactory({ isRed: false });
-    newLeft.replaceWith(newNode.left);
-    newRight.replaceWith(newNode.right);
-    newNode.left = newLeft;
-    newNode.right = newRight;
+    newLeft.replaceWith(thisNode.left);
+    newRight.replaceWith(thisNode.right);
+    thisNode.left = newLeft;
+    thisNode.right = newRight;
 
-    console.log(newNode);
-    return newNode;
+    this.insertRepairTree(thisNode);
+
+    return thisNode;
   }
 
   get NodeClass() {
--- test/insert/description.js
+++ test/insert/description.js
@@ -1,10 +1,18 @@
+const crypto = require('crypto');
+
 module.exports = () => {
   it("Maintains invariants of a red-black tree",() => {
     let uut = new RedBlackTree();
     assert.strictEqual(uut.root.isBlack,true);
     assert.strictEqual(uut.root.isLeaf(),true);
-
-    uut.insert(1,'a');
+    
+    for (let i of [...Array(100).keys()]) {
+      const key = crypto.randomBytes(16).toString("hex");
+      uut.insert(key,key);
+      if (!uut.isValid()) {
+        console.log(uut);
+      }
+    }
   });
 };
 
